cafb596983e745b691ebc652e09822e4a6d6afec
laguiche <ste.guichard@gmail.com>
2025-05-28 17:08:13 +0200

    Nouvelle séquence d'autotest pour CDR2025. Tous les actionneurs sont mis à leur état par défaut à la mise sous tension.

--- Soft_STM32/CM7/Modelia/sm_autotest.cpp
+++ Soft_STM32/CM7/Modelia/sm_autotest.cpp
@@ -1,68 +1,125 @@
-#include "CGlobale.h"
+/**
+ * Generated 28_05_2025 at 17_02
+ */
+
 #include "sm_autotest.h"
+#include "CGlobale.h"
 
 SM_Autotest::SM_Autotest()
 {
+	m_main_mission_type = true;
+	m_max_score = 0;
 }
 
 const char* SM_Autotest::getName()
 {
-    return "SM_Autotest";
+	return "SM_Autotest";
 }
 
 const char* SM_Autotest::stateToName(unsigned short state)
 {
-    switch(state)
-    {
-    //case ALLER_VERS_PHARE :         return "ALLER_VERS_PHARE";
-    }
-    return "UNKNOWN_STATE";
+	switch(state)
+	{
+		case STATE_1 :		return "STATE_1";
+		case STATE_2 :		return "STATE_2";
+		case STATE_3 :		return "STATE_3";
+		case STATE_4 :		return "STATE_4";
+		case STATE_5 :		return "STATE_5";
+		case STATE_6 :		return "STATE_6";
+		case STATE_7 :		return "STATE_7";
+		case STATE_8 :		return "STATE_8";
+		case FIN_MISSION :	return "FIN_MISSION";
+	}
+	return "UNKNOWN_STATE";
 }
-
-
 
 // _____________________________________
 void SM_Autotest::step()
 {
-    switch(m_state)
-    {
-    // ___________________________________
-    case STATE_1 :
-        if (onEntry()) {
-            //on sort le kmar
-            //Application.m_kmar.start(MOUVEMENT_APPRENTISSAGE_CHARIOT);
-        }
-        gotoStateIfConvergenceKmar(m_state+1, 1000);
-        if (onExit()) {
-        }
-        break;
-    // ___________________________________
-    case STATE_2 :
-        if (onEntry()) { }
-        gotoStateAfter(m_state+1, 1000);
-        if (onExit()) { }
-        break;
-        // ___________________________________
-    case STATE_3 :
-        if (onEntry()) {
-            //on initialise les butées du rack
-            //Application.m_asservissement_chariot.Recal_Chariot();
-        }
-        gotoStateIfConvergenceRack(SM_STATE_END, 1000);
-        if (onExit()) {
-            //on range le kmar
-            //Application.m_kmar.start(MOUVEMENT_INIT);
-        }
-        break;
+	switch (m_state)
+	{
 
-    // ___________________________________
-    case SM_STATE_END :
-        if (onEntry()) {
-            gotoState(STATE_1);  // Autorise la machine d'état à être relancée au début
-            stop();
-        }
+	// ___________________________
+	case STATE_1 :
+		if (onEntry()) {
+			
+            //TODO: On monte l'ascenseur à la main en attendant que Simulia le gère
+            //Application.m_ascenseur.up();
 
-        if (onExit()) { }
-        break;
-    }
+		}
+
+			gotoStateAfter(STATE_2,40);
+		if (onExit()) {  }
+		break;
+	// ___________________________
+	case STATE_2 :
+		if (onEntry()) {
+			Application.m_servos.CommandePositionVitesse(2,0,255);/*TODO SERVO_PINCE_ARG values=SERVO_PINCE_ARG_OUVERTE*/
+		}
+
+			gotoStateAfter(STATE_3,40);
+		if (onExit()) {  }
+		break;
+	// ___________________________
+	case STATE_3 :
+		if (onEntry()) {
+			Application.m_servos.CommandePositionVitesse(6,0,255);/*TODO SERVO_PINCE_ARD values=SERVO_PINCE_ARD_OUVERTE*/
+		}
+
+			gotoStateAfter(STATE_4,40);
+		if (onExit()) {  }
+		break;
+	// ___________________________
+	case STATE_4 :
+		if (onEntry()) {
+			Application.m_servos.CommandePositionVitesse(7,2000,255);/*SERVO_VERIN values=VERRIN_POSITION_HAUT*/
+		}
+
+			gotoStateAfter(STATE_5,40);
+		if (onExit()) {  }
+		break;
+	// ___________________________
+	case STATE_5 :
+		if (onEntry()) {
+			Application.m_servos.CommandePositionVitesse(1,2350,255);/*SERVO_PINCE_PLANCHE values=SERVO_PINCE_PLANCHE_REPOS*/
+		}
+
+			gotoStateAfter(STATE_6,40);
+		if (onExit()) {  }
+		break;
+	// ___________________________
+	case STATE_6 :
+		if (onEntry()) {
+			Application.m_servos.CommandePositionVitesse(3,1500,255);/*SERVO_CAN_MOVER_INT values=SERVO_CAN_MOVER_INT_ON*/
+		}
+
+			gotoStateAfter(STATE_7,40);
+		if (onExit()) {  }
+		break;
+	// ___________________________
+	case STATE_7 :
+		if (onEntry()) {
+			Application.m_servos.CommandePositionVitesse(4,0,255);/*TODO SERVO_CAN_TODO MOVER_EXT values=SERVO_CAN_MOVER_EXT_ON*/
+		}
+
+			gotoStateAfter(STATE_8,40);
+		if (onExit()) {  }
+		break;
+	// ___________________________
+	case STATE_8 :
+		if (onEntry()) {
+			Application.m_servos.CommandePositionVitesse(5,1800,255);/*TODO SERVO_BANDEROLE values=SERVO_BANDEROLE_RANGEE*/
+		}
+
+			gotoStateAfter(FIN_MISSION,40);
+		if (onExit()) {  }
+		break;
+
+	// ___________________________
+	case FIN_MISSION :
+		m_succes = true;
+		m_score = m_max_score;
+		stop();
+		break;
+	}
 }
